85c087fe86de4b41656ed04b1a44e586bf9e3e0d
P4uleira <pricardoporfirio2001@gmail.com>
2026-02-11 07:50:13 -0300

    Criação de filmes, visualização de filmes e paginação.

---
+++ FilmesApi/FilmesApi/Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O titulo é obrigatório")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O genero é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho nao pode ser maior que 50 caracteres")]
+    public string Genero { get; set; }
+
+    [Required]
+    [Range(70, 600, ErrorMessage = "A duracao deve ter entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+
+}
